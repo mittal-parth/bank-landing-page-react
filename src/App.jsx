22f5118b3ac727ecd04ec8b5d66341467b2483c7
Parth <mittalparth22@gmail.com>
2022-09-09 00:06:38 +0530

    make the structure of all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './style';
+import { Navbar, Hero, Stats, Business, Billing, CardDeal,
+  Testimonials, Clients, CTA, Footer } from './components';
 
 const App = () => {
   return (
@@ -7,26 +9,26 @@ const App = () => {
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar/>
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`my-12 ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero/>
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart} ${styles.flexStart} `}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CarDeal
-          Testimonials
-          Clients
-          CTA
-          Footer 
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
         </div>
       </div>
     </div>
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+// This file just imports all components and
+// exports them from one place to make code more readbale
+import Billing  from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Navbar,
+    Billing,
+    Button,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    GetStarted,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+};
