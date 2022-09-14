a04be77959d70bac4681f68cc1f502f0b04641dc
Parth <mittalparth22@gmail.com>
2022-09-14 23:07:28 +0530

    add testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import { feedback } from "../constants";
+import styles from "../style";
+import { quotes } from "../assets";
+
+const FeedbackCard = ({ content, name, title, img }) => {
+  return (
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img
+        src={quotes}
+        alt="quotes"
+        className="w-[42px] h-[27px] object-contain"
+      />
+      <p className="font-poppins font-normal text-[18px] text-white my-10 leading-[32px]">
+        {content}
+      </p>
+
+      <div className="flex flex-row">
+        <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full" />
+        <div className="flex flex-col ml-4">
+          <h4 className="font-poppins font-semibold text-[20px] text-white leading-[32px]">
+            {name}
+          </h4>
+          <p className="font-poppins font-normal text-[16px] text-dimWhite my-10 leading-[24px]">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section
+      id="clients"
+      className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
+    >
+      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
-export default Testimonials
+      <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+        <h1 className={styles.heading2}>
+          What are <br className="sm:block hidden" />
+          saying about us
+        </h1>
+        <div className="w-full md:mt-0 mt-6">
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis,
+            earum reprehenderit voluptatem distinctio pariatur minus
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative-z-[1]">
+        {feedback.map((card) => (
+          <FeedbackCard key={card.id} {...card} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
